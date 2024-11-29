06104d8badf0d88490e83edddd3e45577ac45730
Gurusaran22 <guruelangovan22@gmail.com>
2024-11-29 19:06:52 +0530

    added changes

--- src/main/java/com/guru/depend/repository/ResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.guru.depend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.guru.depend.entity.Result;
-
-public interface ResultRepository extends JpaRepository<Result,Long>{
-
-}
